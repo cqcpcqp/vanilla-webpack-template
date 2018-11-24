28f7187d44075395d52415f2d030bda45aa703a4
maximetouroute <maxime.touroute@gmail.com>
2018-11-24 23:34:09 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# vanilla-webpack-template
-A basic webpack template to kickstart a vanillaJS project
+
+
+### Summary
+
+A webpack template for the most basic vanillaJS webapp.
+
+* Bundles js (with babel transpilation)
+* Bundles all kind of assets (css,images,...)
+* generates an index.html based on the template provided "src/index.html"
+
+### Set-up
+
+* `npm install`
+* `npm start` for build and dev server
+
+### to do
+* production build configuration
---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        app: './src/index.js',
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+            contentBase: path.join(__dirname, 'dist'),
+            port: 3010
+    },
+
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        })
+    ],
+
+    module: {
+
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(csv|tsv)$/,
+                use: [
+                    'csv-loader'
+                ]
+            },
+            {
+                test: /\.xml$/,
+                use: [
+                    'xml-loader'
+                ]
+            }
+        ]
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+};
